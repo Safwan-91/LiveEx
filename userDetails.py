e1e51501f4d20aee3f88f63892313a3002b0a9ae
Safwan-91 <safwanpc91@gmail.com>
2024-03-04 21:25:05 +0530

    changes for positional

--- userDetails.py
+++ userDetails.py
@@ -1,13 +1,22 @@
 activeUsers = ["shoonya"]
 users = {
+    # "shoonya": {
+    #     "broker": "shoonya",
+    #     "userid": "FA127352",
+    #     "password": "Messi@2020",
+    #     "vendorCode": "FA127352_U",
+    #     "secret": "a2e650f7d642a160d3d428f6795c0b20",
+    #     "2fa": "MHG22VYK4B2I56KS4X567N2UG7M363A3",
+    #     "imei": "12-mk"
+    # },
     "shoonya": {
         "broker": "shoonya",
-        "userid": "FA127352",
-        "password": "Messi@2020",
-        "vendorCode": "FA127352_U",
-        "secret": "a2e650f7d642a160d3d428f6795c0b20",
-        "2fa": "MHG22VYK4B2I56KS4X567N2UG7M363A3",
-        "imei": "12-mk"
+        "userid": "FA227991",
+        "password": "FaiSal@12",
+        "vendorCode": "FA227991_U",
+        "secret": "9fbf6b797b8f967d04b427fee8d16886",
+        "2fa": "34477UN26JMYE35NU3DLT4M7LU464267",
+        "imei": "abc1234"
     },
     "kotak": {
         "broker": "kotakNeo"
