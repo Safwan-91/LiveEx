88dbbae19361b049cc1f9d1348fbc68c84719bc1
Administrator <safwanpc91@gmail.com>
2024-03-17 18:15:22 +0530

    parameterised exchange

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import Utils
 import liveUtils
 from runLive import Live
 from datetime import datetime
-from Users import User
 from pyIB_APIS import IB_APIS
 
 
 if __name__ == '__main__':
     client = IB_APIS("http://localhost:21000")
-    client.IB_Subscribe("NSE", Utils.indexToken, "")
+    client.IB_Subscribe(Utils.indexExchange, Utils.indexToken, "")
     runLive = Live(Utils.indexToken, client)
     currMin = None
     while "09:15:00" <= datetime.now().strftime("%H:%M:%S") <= "15:30:00":
