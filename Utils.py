1ba75d31699814033222a342f0cade17053e4f7d
Administrator <safwanpc91@gmail.com>
2024-03-20 13:54:38 +0530

    mtm exit fix

--- Utils.py
+++ Utils.py
@@ -6,7 +6,7 @@ initialPremiumMap = {"MIDCPNIFTY": 25, "FINNIFTY": 40, "BANKNIFTY": 100, "NIFTY"
 mtmSLMap = {"MIDCPNIFTY": 10, "FINNIFTY": 30, "BANKNIFTY": 60, "NIFTY": 30, "SENSEX": 70, "BANKEX": 60}
 strikeDifferenceMap = {"MIDCPNIFTY": 25, "FINNIFTY": 50, "BANKNIFTY": 100, "NIFTY": 50, "SENSEX": 100, "BANKEX": 100}
 indexTokenMap = {"MIDCPNIFTY": "NIFTY MID SELECT", "FINNIFTY": "NIFTY FIN SERVICE", "BANKNIFTY": "BANKNIFTY", "NIFTY": "NIFTY", "SENSEX": "SENSEX", "BANKEX": "BANKEX"}
-lotSizeMap = {"MIDCPNIFTY": 75, "FINNIFTY": 40, "BANKNIFTY": 15, "NIFTY": 50, "SENSEX": 10, "BANKEX": 15}
+lotSizeMap = {"MIDCPNIFTY": 1, "FINNIFTY": 3, "BANKNIFTY": 4, "NIFTY": 2, "SENSEX": 3, "BANKEX": 2}
 
 indexToken = indexTokenMap[index]
 lotSize = lotSizeMap[index]
