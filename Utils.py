590d6ab705edbbb516af077cfaeaf422cd5202d3
Safwan-91 <safwanpc91@gmail.com>
2024-05-09 22:48:26 +0530

    initial changes

--- Utils.py
+++ Utils.py
@@ -1,13 +1,6 @@
 import logging
 from datetime import datetime
-
-logPath = "C:/Users/Administrator/Desktop/logs/"
-formatter = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-logging.basicConfig(filename=logPath+datetime.now().strftime('%Y-%m-%d')+'.log', level=logging.DEBUG, format=formatter)
-logger = logging.getLogger(__name__)
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(logging.Formatter(formatter))
-logger.addHandler(console_handler)
+from concurrent.futures import ThreadPoolExecutor
 
 index = "FINNIFTY"
 expDate = "24507"
@@ -17,7 +10,15 @@ startTime = "10:44:00"
 adjSL = 2
 strategyTag = "OPTIONSPLAY1045SL2"
 
-adjustmentShift = "True"
+executor = ThreadPoolExecutor()
+
+logPath = "C:/Users/Administrator/Desktop/logs/"
+formatter = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(filename=logPath+datetime.now().strftime('%Y-%m-%d')+'.log', level=logging.DEBUG, format=formatter)
+logger = logging.getLogger(__name__)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(logging.Formatter(formatter))
+logger.addHandler(console_handler)
 
 initialPremiumMap = {"MIDCPNIFTY": 25, "FINNIFTY": 40, "BANKNIFTY": 110, "NIFTY": 50, "SENSEX": 163, "BANKEX": 125}
 mtmSLMap = {"MIDCPNIFTY": 10, "FINNIFTY": 25, "BANKNIFTY": 70, "NIFTY": 30, "SENSEX": 100, "BANKEX": 80}
@@ -26,6 +27,7 @@ indexTokenMap = {"MIDCPNIFTY": "NIFTY MID SELECT", "FINNIFTY": "NIFTY FIN SERVIC
 lotSizeMap = {"MIDCPNIFTY": 1, "FINNIFTY": 3, "BANKNIFTY": 3, "NIFTY": 4, "SENSEX": 3, "BANKEX": 3}
 shiftAmountMap = {"MIDCPNIFTY": 1, "FINNIFTY": 1, "BANKNIFTY": 2, "NIFTY": 1, "SENSEX": 2, "BANKEX": 2}
 
+adjustmentShift = "True"
 shiftAmount = shiftAmountMap[index] if adjSL == 1 else shiftAmountMap[index] + 1
 indexToken = indexTokenMap[index]
 lotSize = lotSizeMap[index]
